dba2a588d684ca5d03ae5736b6f772bf86ef0942
nbintang <nbintangh@gmail.com>
2026-01-16 03:24:14 +0700

    feat: add some ascii logo thingy lol

--- cmd/root.go
+++ cmd/root.go
@@ -6,12 +6,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd =  &cobra.Command{
+var rootCmd = &cobra.Command{
 	Use:   "goscaff",
 	Short: "A Go CLI tool to scaffold backend projects",
-	Long: `goscaff is an opinionated Go scaffolding CLI.
-	Use it to generate a production-ready Go backend boilerplate.`,
-	RunE: func(cmd *cobra.Command, args []string) error { 
+	Long: `
+                                            
+ ▄████   ▄▄▄   ▄▄▄▄  ▄▄▄▄  ▄▄▄  ▄▄▄▄▄ ▄▄▄▄▄ 
+██  ▄▄▄ ██▀██ ███▄▄ ██▀▀▀ ██▀██ ██▄▄  ██▄▄  
+ ▀███▀  ▀███▀ ▄▄██▀ ▀████ ██▀██ ██    ██    
+                                            
+Goscaff is an instant Go scaffolding CLI.
+Use it to generate a production-ready Go backend boilerplate.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmd.Help()
 	},
 }
